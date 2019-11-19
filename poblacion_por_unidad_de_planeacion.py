5e918219bba155336e08a200442a2196fc0608a8
finkej <jorgefinke@gmail.com>
2019-11-19 18:24:41 -0500

    updated census data

--- poblacion_por_unidad_de_planeacion.py
+++ poblacion_por_unidad_de_planeacion.py
@@ -1,13 +1,33 @@
 import pandas as pd
 import geopandas as gpd
+import geoplot
 
-#import data data from census
-poblacion_data_link='https://raw.githubusercontent.com/finkej/bogota/master/estadisticas_poblacion/Informaci_n_poblacional_Encuesta_Multiprop_sito_Bogot__2017.csv'
-df=pd.read_csv(poblacion_data_link)
 
-gpd.GeoDataFrame(df).rename({'UPZ':'UPlCodigo'})
+#import data from census
+df1=pd.read_csv('./encuestas/encuesta_multiproposito_bogota_2017.csv')
+df1.rename(columns={'UPZ':'UPlCodigo'}, inplace=True)
+#gdf1=(gpd.GeoDataFrame(df)).rename(columns={'UPZ':'UPlCodigo'})
+#import UP data
+gdf=gpd.read_file('./mapa_bogota/unidad_de_planeacion/unidad_de_planeacion.shp')
+df2=pd.DataFrame(gdf)
+df=pd.merge(df1, df2, on='UPlCodigo')
+df.columns=df.columns.str.lower().str.replace(' ','_').str.replace('(','').str.replace(')','').str.replace('ñ','n').str.replace('ó','o')
 
-gdf=gpd.read_file('mapa_bogota/unidad_de_planeacion/unidad_de_planeacion.shp')
+for column in df.columns:
+    if (type(df[column][0]) is str):
+        df[column]=df[column].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8').str.lower()
+  
+
+gdf=gpd.GeoDataFrame(df)
+
+geoplot.choropleth(
+    gdf, hue=gdf['personas'],
+    cmap='Greens', figsize=(8, 4)
+)
+
+gdf.to_file(driver = 'ESRI Shapefile', filename = './encuestas/encuesta_multiproposito_bogota_2017.shp')
+
+#gdf=gpd.read_file('./encuestas/encuesta_multiproposito_bogota_2017.shp')
 
 # create city boundaries
 #crs = {'init': 'epsg:4326'}
@@ -17,5 +37,3 @@ gdf=gpd.read_file('mapa_bogota/unidad_de_planeacion/unidad_de_planeacion.shp')
 # gdf['city']='bogota'
 # limite_municipal=gdf.dissolve(by='city')
 # limite_municipal.to_file(driver = 'ESRI Shapefile', filename = './mapa_bogota/limite_municipal_urbano/limite_municipal_urbano.shp')
-
-
